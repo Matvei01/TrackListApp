ce61e5b57403e37e2ae13e132d0cc2fcc02df877
Matvei95 <khlestov.95@mail.ru>
2022-06-03 20:32:10 +0500

    Working with code and interface builder.

---
+++ TrackListApp/TrackDetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TrackDetailsViewController.swift
+//  TrackListApp
+//
+//  Created by Matvei Khlestov on 02.06.2022.
+//
+
+import UIKit
+
+class TrackDetailsViewController: UIViewController {
+
+    @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    
+    var track: Track!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        coverImageView.image = UIImage(named: track.title)
+        trackNameLabel.text = track.title
+    }
+}
---
+++ TrackListApp/TrackListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TrackListViewController.swift
+//  TrackListApp
+//
+//  Created by Matvei Khlestov on 02.06.2022.
+//
+
+import UIKit
+
+class TrackListViewController: UITableViewController {
+    
+    private var trackList = Track.getTrackList()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 80
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        trackList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "trackID", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let track = trackList[indexPath.row]
+        
+        content.text = track.song
+        content.secondaryText = track.artist
+        content.image = UIImage(named: track.title)
+        content.imageProperties.cornerRadius = tableView.rowHeight / 2
+        
+        cell.contentConfiguration = content
+
+        return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let detailsVC = segue.destination as? TrackDetailsViewController else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        let track = trackList[indexPath.row]
+        detailsVC.track = track
+    }
+}
